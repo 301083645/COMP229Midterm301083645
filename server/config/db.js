94d1bea55e23db086944246b4d7b9a6b515e9172
301083645 <eblee150@gmail.com>
2020-10-26 13:58:37 -0400

    Final

---
+++ server/config/db.js
@@ -0,0 +1,6 @@
+module.exports = {
+  //local MongoDB deployment ->
+  //"URI": "mongodb://localhost/books229"
+  "URI": "mongodb+srv://student301083645:H5HODXtJQDIt7hnG@cluster0.48jht.mongodb.net/booklist?retryWrites=true&w=majority"
+
+};
